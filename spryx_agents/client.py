194c8163388426d1743e1deccb06eabe531ffa79
Pedro Cantidio <ppcantidio@gmail.com>
2025-05-19 23:02:46 -0300

    feat(client): add embedding_usages resource to SpryxAgents

--- spryx_agents/client.py
+++ spryx_agents/client.py
@@ -4,6 +4,7 @@ from spryx_agents.resources.agents import Agents
 from spryx_agents.resources.chats import Chats
 from spryx_agents.resources.credential_types import CredentialTypes
 from spryx_agents.resources.credentials import Credentials
+from spryx_agents.resources.embedding_usages import EmbeddingUsages
 from spryx_agents.resources.files import Files
 from spryx_agents.resources.integrations import Integrations
 from spryx_agents.resources.vector_stores import VectorStores
@@ -31,3 +32,4 @@ class SpryxAgents(SpryxAsyncClient):
         self.files = Files(self)
         self.integrations = Integrations(self)
         self.vector_stores = VectorStores(self)
+        self.embedding_usages = EmbeddingUsages(self)
